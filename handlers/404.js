6b0f10ff54148624c78a139ec209aa4ab11a6135
shabitee <94993051+shabitee@users.noreply.github.com>
2023-09-27 13:20:43 +0100

    added server.js

--- handlers/404.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-module.exports = (req,res,next)=>{
-    res.status(404).json({
-        code:404,
-        message:'Route not found'
-    })
-}
--- handlers/500.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-module.exports = (error,req,res,next)=>{
-    res.status(500).send({
-        code:500,
-        message:`Server error ${error}`
-        })
-}
