e8e232dc3e8d652232cb9366edbf10c4e43c1c22
shabitee <94993051+shabitee@users.noreply.github.com>
2023-09-27 13:23:23 +0100

    added hadithFunaction under controllers

---
+++ controllers/hadithFunction.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+require("dotenv").config();
+const hadithModel = require('../models/scheModel');
+
+async function getAllHadithApiHandler(req,res){
+    let allHadithApi = await axios.get(`https://hadithapi.com/public/api/hadiths?apiKey=${process.env.API_KEY}&paginate=100`);
+    console.log(allHadithApi.data.hadiths.data[0])
+    res.send(allHadithApi.data.hadiths.data);
+}
