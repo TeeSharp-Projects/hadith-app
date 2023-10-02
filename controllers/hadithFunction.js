40be9a78c19fa4d928a0f3be4ed573528231cc59
shabitee <94993051+shabitee@users.noreply.github.com>
2023-10-02 13:34:23 +0100

    Added getAllHadithApiHandler

--- controllers/hadithFunction.js
+++ controllers/hadithFunction.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const axios = require('axios');
 require("dotenv").config();
 const hadithModel = require('../models/scheModel');
@@ -6,4 +8,15 @@ async function getAllHadithApiHandler(req,res){
     let allHadithApi = await axios.get(`https://hadithapi.com/public/api/hadiths?apiKey=${process.env.API_KEY}&paginate=100`);
     console.log(allHadithApi.data.hadiths.data[0])
     res.send(allHadithApi.data.hadiths.data);
+}
+
+
+
+
+module.exports ={
+    getAllHadithApiHandler,
+    // getAllHadithToDatabaseHandler,
+    // addHadithInDatabaseHandler,
+    // delHadithInDatabaseHandler, 
+
 }
